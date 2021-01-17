1cbbcf1e782285a21f8ee7b1b1991dccffccc9c9
Faisal Masood <fmasood@redhat.com>
2021-01-18 08:42:29 +1100

    class 3 with expressions and tge rtpe conversion :heart_eyes:

---
+++ expressions.py
@@ -0,0 +1,31 @@
+# This program will add two numbers
+
+# first_number = 5
+# second_number = 6
+#
+# #What this will do?
+# #10 = third_number
+#
+# sum_of_numbers = first_number + second_number
+# print(sum_of_numbers)
+#
+first_number = "5"
+second_number = "6"
+sum_of_numbers = first_number + second_number
+print(sum_of_numbers)
+
+first_number = "5"
+# second_number = "6"
+# print(type(first_number))
+
+# Convert types
+first_number_integer = int(first_number)
+print(type(first_number_integer))
+
+first_number_string = str(first_number_integer)
+print(type(first_number_string))
+
+first_number_is_five = first_number_integer == 5
+print(first_number_is_five)
+print(type(first_number_is_five))
+
---
+++ user_input.py
@@ -0,0 +1,13 @@
+# User input
+first_number = float(input("Please enter first number: "))
+second_number = int(input("Please enter second number:"))
+sum_of_numbers = first_number + second_number
+print("Type of the number is ", type(first_number))
+
+# The output format is "The sum is NUMBER"
+print("The sum is", sum_of_numbers)
+
+# The output format is as follows
+# "The sum of FIRST_NUMBER and SECOND_NUMBER is NUMBER"
+# print("The sum of", first_number, "and", second_number, "is", sum_of_numbers)
+print(f"The sum of {first_number} and {second_number} is {sum_of_numbers}")
