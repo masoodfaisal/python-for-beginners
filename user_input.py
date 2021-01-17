4ef60a9b4cb8cc43d3407b1a688a5cd70af546e6
Faisal Masood <fmasood@redhat.com>
2021-01-18 08:43:34 +1100

    add more comments :monkey:

--- user_input.py
+++ user_input.py
@@ -2,6 +2,11 @@
 first_number = float(input("Please enter first number: "))
 second_number = int(input("Please enter second number:"))
 sum_of_numbers = first_number + second_number
+
+# - SUBTRACTION
+# * MULTIPLE
+# / DIVISION
+
 print("Type of the number is ", type(first_number))
 
 # The output format is "The sum is NUMBER"
