a8deb670daf4e670b2ec8830dc38f335b4a67aab
Faisal Masood <fmasood@redhat.com>
2021-02-15 08:35:36 +1100

    function. basics

---
+++ function_first.py
@@ -0,0 +1,18 @@
+
+
+# our first function
+def printInformation():
+    print("This is function example")
+
+# a function with parameter
+def sayHello(name):
+    print(f"Hello {name}. I hope you are good today")
+
+# printInformation() 
+sayHello("Anisa")  
+sayHello("Omer")  
+sayHello("Ammar")  
+sayHello("Misbah")  
+
+
+# Define a SUM function, which takes two parameters and print the sum of them
